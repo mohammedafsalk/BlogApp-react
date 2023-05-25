16e6f011232226c637376692710e72fd393c843b
mohammedafsalk <techafs456@gmail.com>
2023-05-25 11:12:31 +0530

    changes made

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,28 @@
+import { useState } from "react";
+
 const Home = () => {
+  const [item, setItem] = useState([
+    {
+      title: "My New Website",
+      body: "lorem ipsum....",
+      author: "Mario",
+      id: 1,
+    },
+    { title: "My New Cookie", body: "lorem ipsum....", author: "Dev", id: 2 },
+    { title: "My New Table", body: "lorem ipsum....", author: "Claw", id: 3 },
+  ]);
+
+  const handleclick = (title) => {
+    setItem(title);
+  };
   return (
     <div className="home">
-      <h2>Homepage</h2>
+      {item.map((item) => (
+        <div className="blog-preview">
+        <h2>{item.title}</h2>
+        <p>{item.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
