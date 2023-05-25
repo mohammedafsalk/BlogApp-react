4345f3eec1d70e1771f3446f02cb86490c635084
mohammedafsalk <techafs456@gmail.com>
2023-05-25 12:55:54 +0530

    fetching added

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,30 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import List from "./List";
 
 const Home = () => {
-  const [item, setItem] = useState([
-    {
-      title: "My New Website",
-      body: "lorem ipsum....",
-      author: "Mario",
-      id: 1,
-    },
-    { title: "My New Cookie", body: "lorem ipsum....", author: "Dev", id: 2 },
-    { title: "My New Table", body: "lorem ipsum....", author: "Mario", id: 4 },
-    { title: "My New Bike", body: "lorem ipsum....", author: "Claw", id: 5 },
-    { title: "My New Google", body: "lorem ipsum....", author: "Dev", id: 6 },
-    {
-      title: "My New Instgram",
-      body: "lorem ipsum....",
-      author: "Claw",
-      id: 7,
-    },
-  ]);
+  const [item, setItem] = useState([null]);
 
   const deleteHandler = (id) => {
     const newBlog = item.filter((item) => item.id !== id);
     setItem(newBlog);
   };
+
+  useEffect(()=>{
+
+  },[])
 
   return (
     <div className="home">
