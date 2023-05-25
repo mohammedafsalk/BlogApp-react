504f2b18abc90f5affff89e2bae82f1779ac5a62
mohammedafsalk <techafs456@gmail.com>
2023-05-25 20:10:28 +0530

    adding a new item added

--- src/Components/Create.js
+++ src/Components/Create.js
@@ -1,10 +1,53 @@
+import { useState } from "react";
 
 function Create() {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("");
+  const [pending,isPending] = useState(false)
+
+  const submithandler = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+    isPending(true)
+
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log("new blog added");
+      isPending(false)
+    });
+  };
+
   return (
     <div className="create">
-        <h2>Add New Blog</h2>
+      <h2>Add a New Blog</h2>
+      <form onSubmit={submithandler}>
+        <label>Blog title:</label>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+        />
+        <label>Blog body:</label>
+        <textarea
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+          required
+        ></textarea>
+        <label>Blog author:</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="mario">mario</option>
+          <option value="yoshi">yoshi</option>
+        </select>
+        {!pending && <button>Add Blog</button>}
+        {pending && <button disabled>Adding..</button>}
+      </form>
     </div>
-  )
+  );
 }
 
-export default Create
+export default Create;
